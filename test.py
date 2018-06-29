14321d6e60beeb2e75e0520ac9eec7e3cc0e8cec
James <james@akaintelligence.com>
2018-06-29 14:53:59 +0900

    Add a function to drink

--- test.py
+++ test.py
@@ -4,7 +4,11 @@ def hello():
 def would_you_like_to_have_a_dinner():
     return "YES"
 
+def how_about_having_a_bottle_of_beer():
+    return "Great"
+
 
 if __name__ == "__main__":
     hello()
     print(would_you_like_to_have_a_dinner())
+    print(how_about_having_a_bottle_of_beer())
